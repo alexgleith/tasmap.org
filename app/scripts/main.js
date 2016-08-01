4ee3073e2a528d86f8fa4e7016285f81029bc790
Alex Leith <alex.leith@tidetech.org>
2016-08-01 14:35:24 +1000

    Bug fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -55,7 +55,6 @@ $(document).on("click", ".data-table tr", function(e) {
     try {
       map.fitBounds(layer.getBounds());
     } catch (TypeError) {
-      console.log(layer)
       map.panTo(layer._latlng)
     }
   }
@@ -608,23 +607,27 @@ function removeParameterByName (name, url) {
 //from http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
 function linkify(inputText) {
     if(!inputText) {
-      return
+      return "Null"
     }
-    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+    try {
+      var replacedText, replacePattern1, replacePattern2, replacePattern3;
 
-    //URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+      //URLs starting with http://, https://, or ftp://
+      replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+      replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+      //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+      replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+      replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-    //Change email addresses to mailto:: links.
-    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+      //Change email addresses to mailto:: links.
+      replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+      replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 
-    return replacedText;
+      return replacedText;
+    } catch (TypeError) {
+      return inputText
+    }
 }
 
 // Hash the location
