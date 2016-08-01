4d084e6e4772d4f13016551b71943dd71b9e31ec
Alex Leith <alex.leith@tidetech.org>
2016-08-01 19:15:58 +1000

    Fix the location thing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -150,35 +150,25 @@ map = L.map('map', {
 
 /* GPS enabled geolocation control set to follow the user's location */
 var locateControl = L.control.locate({
-  position: 'topleft',
-  drawCircle: true,
-  follow: true,
-  setView: true,
-  keepCurrentZoomLevel: true,
-  markerStyle: {
-    weight: 1,
-    opacity: 0.8,
-    fillOpacity: 0.8
-  },
-  circleStyle: {
-    weight: 1,
-    clickable: false
-  },
-  icon: 'fa fa-location-arrow',
-  metric: true,
-  strings: {
-    title: 'My location',
-    popup: 'You are within {distance} {unit} from this point',
-    outsideMapBoundsMsg: 'You seem located outside the boundaries of the map'
-  },
-  locateOptions: {
-    maxZoom: 18,
-    watch: true,
-    enableHighAccuracy: true,
-    maximumAge: 10000,
-    timeout: 10000
-  }
-}).addTo(map)
+    position: 'topleft',
+    markerStyle: {
+      weight: 1,
+      opacity: 0.8,
+      fillOpacity: 0.8
+    },
+    circleStyle: {
+      weight: 1,
+      clickable: false
+    },
+    icon: 'fa fa-location-arrow',
+    metric: true,
+    strings: {
+      title: 'My location',
+      popup: 'You are within {distance} {unit} of this point',
+      outsideMapBoundsMsg: 'You seem located outside the boundaries of the map'
+    }
+}).addTo(map);
+
 
 /* Larger screens get expanded layer control and visible sidebar */
 if (document.body.clientWidth <= 767) {
