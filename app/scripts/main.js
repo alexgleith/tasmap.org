38fc5168d110c66e53bd6f8e69221a6a298371d5
Alex Leith <alex.leith@tidetech.org>
2016-08-01 13:26:50 +1000

    Make clicky links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -447,7 +447,7 @@ function addDataToTable(title, id, data) {
     tableContent += '<tr id=' + data[i].uuid + '>'
     tableContent += '<td style="display:none">' + data[i].uuid + '</td>'
     for (var name in oneRow.properties) {
-      tableContent += '<td>' + oneRow.properties[name] + '</td>'
+      tableContent += '<td>' + linkify(oneRow.properties[name]) + '</td>'
     };
     tableContent += '</tr>'
   }
@@ -603,6 +603,25 @@ function removeParameterByName (name, url) {
     rtn = rtn + '?' + params_arr.join('&')
   }
   window.history.replaceState({}, '', rtn + hash)
+}
+
+//from http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
 }
 
 // Hash the location
